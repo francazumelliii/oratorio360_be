b92a92edb81d2e30ba3a27633a3eecbf55e7b6b2
francazumelliii <frazumelli04@gmail.com>
2025-02-04 11:05:23 +0100

    [NEW] AbstractModel for redundant fields

---
+++ src/main/java/com/bezkoder/spring/security/jwt/models/AbstractModel.java
@@ -0,0 +1,29 @@
+package com.bezkoder.spring.security.jwt.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+public abstract class AbstractModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(name="created_at", nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name="updated_at", nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @Column(name="deleted_at")
+    private Timestamp deletedAt;
+
+
+}
