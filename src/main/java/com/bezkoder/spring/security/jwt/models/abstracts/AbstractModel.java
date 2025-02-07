9193a93c64a9608fb12eaabd2aad4bdaeb3cb52d
francazumelliii <frazumelli04@gmail.com>
2025-02-07 16:35:57 +0100

    [UPDATE] added getters and setters

--- src/main/java/com/bezkoder/spring/security/jwt/models/abstracts/AbstractModel.java
+++ src/main/java/com/bezkoder/spring/security/jwt/models/abstracts/AbstractModel.java
@@ -26,4 +26,35 @@ public abstract class AbstractModel {
     private Timestamp deletedAt;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
